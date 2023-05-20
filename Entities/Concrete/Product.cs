c46c0d0d5c13f5274df66d40d62d66f16a81c033
tugba <tugba@TUBA>
2023-05-21 00:16:33 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Entities.Concrete
+{
+    public class Product : IEntity   // public koyduk satır başına çünkü public koyunca bu classa diğer katmanlarda(business,dataaccsess,consoleuı) ulaşabilir.
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
